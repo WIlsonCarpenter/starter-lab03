dae87b5da4992259007a3affddf379a22e6d7ebc
Wilson Carpenter <wilsonkcarpenter@gmail.com>
2015-09-24 15:00:43 -0700

    Fixed first link

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,24 @@
+<?php
+
+class Welcome extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        
+        $source = $this->quotes->last();
+        $this->data['who'] = $source['who'];
+        $this->data['what'] = $source['what'];
+        $this->data['mug'] = $source['mug'];
+
+        $this->render();
+    }
+
+}
